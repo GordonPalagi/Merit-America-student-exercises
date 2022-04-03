fe3bca917370b9c178951d440165d6bd50f3577e
Gordon <gordonpalagi1@gmail.com>
2022-04-03 15:56:45 -0700

    git comment -m "Submission for word search exercise"

--- 17_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
+++ 17_File_IO_Writing/exercise/src/main/java/com/techelevator/FizzWriter.java
@@ -1,9 +1,41 @@
 package com.techelevator;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class FizzWriter {
 
 	public static void main(String[] args) {
 
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("What is the destination file? ");
+		String filePath = userInput.nextLine();
+		File fileObject = new File(filePath);
+
+
+
+		try (
+				PrintWriter writingFile = new PrintWriter(new FileOutputStream(fileObject, true))
+		) {
+				for (int i = 1; i < 301; i++) {
+					if (i % 3 == 0 && i % 5 == 0) {
+						writingFile.write("FizzBuzz\n");
+					}
+					else if (i % 3 == 0) {
+						writingFile.write("Fizz\n");
+					}
+					else if (i % 5 == 0) {
+						writingFile.write("Buzz\n");
+					}
+					else {
+						writingFile.write(i + "\n");
+					}
+				}
+			} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
 	}
 
-}
